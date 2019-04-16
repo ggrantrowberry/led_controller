f92d8d71cb773f810fc3166fe86ba88ff134c204
ggrantrowberry <ggrantrowberry@gmail.com>
2019-04-16 17:26:36 -0600

    Allowed the build to be put on git

---
+++ front_end/build/precache-manifest.6bacfab0b358e375b56a78380347fc85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "1403ea2bad490a6b5f52",
+    "url": "/static/js/main.e1ceee08.chunk.js"
+  },
+  {
+    "revision": "1b495bedbdb114a3bc5a",
+    "url": "/static/js/2.eb657127.chunk.js"
+  },
+  {
+    "revision": "1403ea2bad490a6b5f52",
+    "url": "/static/css/main.5538874d.chunk.css"
+  },
+  {
+    "revision": "9a9ad50e53d07274d69b75af869feab1",
+    "url": "/index.html"
+  }
+];
